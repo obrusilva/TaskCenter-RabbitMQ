30092aac363d027f2d96e037ce8ce2ea7b8a6cbb
Bruno Silva <bruno.silva@sintesesolucoes.com.br>
2022-01-07 20:01:25 -0300

    Commit Inicial.

---
+++ TaskCenter/PublisherCenter/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+using TaskCenter.Dto.Virtual;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ConnectionFactory factory = new ()
+        {
+            Uri = new Uri("amqp://usertaskcenter:passtaskcenter@127.0.0.1:5672/taskCenter_virtual_host"),
+            NetworkRecoveryInterval = TimeSpan.FromSeconds(10),
+            AutomaticRecoveryEnabled = true
+        };
+
+        using var connection = factory.CreateConnection();  
+
+        using var model  = connection.CreateModel();
+
+        //confirmar o envio da mensagem.
+        model.ConfirmSelect();
+
+        //declara a exchange 
+        model.ExchangeDeclare(exchange:"Task_Client", type:"topic", durable: true,autoDelete:false, arguments: null);
+        //declara a fila
+        model.QueueDeclare(queue:"Task_Client_Company",durable:true,exclusive:false, autoDelete:false,arguments:null);
+        // efetua o bind entre a fila e a exchange
+        model.QueueBind(queue: "Task_Client_Company", exchange: "Task_Client",routingKey:"process.company",arguments:null);
+
+        TaskDetails taskDetails = new() { Company = "Company",  Process = "B2B.PRODUCT", Script = "select  top 10 produto, desc_produto from produtos" };
+
+        string message = JsonSerializer.Serialize(taskDetails); 
+        var body = Encoding.UTF8.GetBytes(message);
+
+        // precisa criar as propriedades pois precisa usar o DeliveryMode = 2 Persiste, 1 Apenas memoria(nessa caso se o rabbit cair as mensagems somem)
+        var props = model.CreateBasicProperties();
+        props.Headers = new Dictionary<string, Object> { { "content-type", "application/json" } };
+        props.DeliveryMode = 2;
+
+        model.BasicPublish(exchange: "Task_Client", routingKey: "process.company",basicProperties:props, body:body);
+
+    }
+}
---
+++ TaskCenter/TaskCenter.Dto/Virtual/TaskDetails.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TaskCenter.Dto.Virtual
+{
+    public class TaskDetails
+    {
+        public string Company { get; set; }
+        public string Process { get; set; }
+        public string Script { get; set; }
+    }
+}
