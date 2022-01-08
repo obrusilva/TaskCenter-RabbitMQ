9e42f433a526e9d07e4b56d615e0ce79a9528359
Bruno Silva <brunopdp@gmail.com>
2022-01-07 21:52:17 -0300

    - Adicionado for para mandar mensagens

--- TaskCenter/PublisherCenter/Program.cs
+++ TaskCenter/PublisherCenter/Program.cs
@@ -7,38 +7,67 @@ class Program
 {
     static void Main(string[] args)
     {
-        ConnectionFactory factory = new ()
+        int counter = 10;    
+
+        for (int i = 0;i < counter; i++)
         {
-            Uri = new Uri("amqp://usertaskcenter:passtaskcenter@127.0.0.1:5672/taskCenter_virtual_host"),
-            NetworkRecoveryInterval = TimeSpan.FromSeconds(10),
-            AutomaticRecoveryEnabled = true
-        };
 
-        using var connection = factory.CreateConnection();  
+            List<TaskDetails> list = new();
+            list.Add(new TaskDetails { Company = "AOC", Process = "MARKETPLACE.PRODUCT", Script = string.Empty });
+            list.Add(new TaskDetails { Company = "APPLE", Process = "MARKETPLACE.PRODUCT", Script = string.Empty });
+            list.Add(new TaskDetails { Company = "SAMSUNG", Process = "MARKETPLACE.PRODUCT", Script = string.Empty });
+            list.Add(new TaskDetails { Company = "LG", Process = "MARKETPLACE.PRODUCT", Script = string.Empty });
+            list.Add(new TaskDetails { Company = "CISCO", Process = "MARKETPLACE.PRODUCT", Script = string.Empty });
+            list.Add(new TaskDetails { Company = "LACOSTE", Process = "MARKETPLACE.PRODUCT", Script = string.Empty });
+            list.Add(new TaskDetails { Company = "GAP", Process = "MARKETPLACE.PRODUCT", Script = string.Empty });
+            list.Add(new TaskDetails { Company = "AOC", Process = "MARKETPLACE.STOCK", Script = string.Empty });
+            list.Add(new TaskDetails { Company = "APPLE", Process = "MARKETPLACE.STOCK", Script = string.Empty });
+            list.Add(new TaskDetails { Company = "SAMSUNG", Process = "MARKETPLACE.STOCK", Script = string.Empty });
+            list.Add(new TaskDetails { Company = "LG", Process = "MARKETPLACE.STOCK", Script = string.Empty });
+            list.Add(new TaskDetails { Company = "CISCO", Process = "MARKETPLACE.STOCK", Script = string.Empty });
+            list.Add(new TaskDetails { Company = "LACOSTE", Process = "MARKETPLACE.STOCK", Script = string.Empty });
+            list.Add(new TaskDetails { Company = "GAP", Process = "MARKETPLACE.STOCK", Script = string.Empty });
 
-        using var model  = connection.CreateModel();
+            ConnectionFactory factory = new()
+            {
+                Uri = new Uri("amqp://usertaskcenter:passtaskcenter@127.0.0.1:5672/taskCenter_virtual_host"),
+                NetworkRecoveryInterval = TimeSpan.FromSeconds(10),
+                AutomaticRecoveryEnabled = true
+            };
 
-        //confirmar o envio da mensagem.
-        model.ConfirmSelect();
+            using var connection = factory.CreateConnection();
 
-        //declara a exchange 
-        model.ExchangeDeclare(exchange:"Task_Client", type:"topic", durable: true,autoDelete:false, arguments: null);
-        //declara a fila
-        model.QueueDeclare(queue:"Task_Client_Company",durable:true,exclusive:false, autoDelete:false,arguments:null);
-        // efetua o bind entre a fila e a exchange
-        model.QueueBind(queue: "Task_Client_Company", exchange: "Task_Client",routingKey:"process.company",arguments:null);
+            using var model = connection.CreateModel();
 
-        TaskDetails taskDetails = new() { Company = "Company",  Process = "B2B.PRODUCT", Script = "select  top 10 produto, desc_produto from produtos" };
+            //confirmar o envio da mensagem.
+            model.ConfirmSelect();
 
-        string message = JsonSerializer.Serialize(taskDetails); 
-        var body = Encoding.UTF8.GetBytes(message);
+            //declara a exchange 
+            model.ExchangeDeclare(exchange: "Task_Client", type: "topic", durable: true, autoDelete: false, arguments: null);
 
-        // precisa criar as propriedades pois precisa usar o DeliveryMode = 2 Persiste, 1 Apenas memoria(nessa caso se o rabbit cair as mensagems somem)
-        var props = model.CreateBasicProperties();
-        props.Headers = new Dictionary<string, Object> { { "content-type", "application/json" } };
-        // Persiste
-        props.DeliveryMode = 2;
+            foreach (var company in list)
+            {
+                //declara a fila
 
-        model.BasicPublish(exchange: "Task_Client", routingKey: "process.company",basicProperties:props, body:body);
+                model.QueueDeclare(queue: $"Task_Client_{company.Company}", durable: true, exclusive: false, autoDelete: false, arguments: null);
+                // efetua o bind entre a fila e a exchange
+                model.QueueBind(queue: $"Task_Client_{company.Company}", exchange: "Task_Client", routingKey: $"{company.Process}.{company.Company}", arguments: null);
+
+                string message = JsonSerializer.Serialize(company);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                // precisa criar as propriedades pois precisa usar o DeliveryMode = 2 Persiste, 1 Apenas memoria(nessa caso se o rabbit cair as mensagems somem)
+                var props = model.CreateBasicProperties();
+                props.Headers = new Dictionary<string, Object> { { "content-type", "application/json" } };
+                // Persiste
+                props.DeliveryMode = 2;
+
+                model.BasicPublish(exchange: "Task_Client", routingKey: $"{company.Process}.{company.Company}", basicProperties: props, body: body);
+            }
+
+            Thread.Sleep(300000);
+        }
+        
     }
 }
